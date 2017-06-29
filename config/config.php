640ec08145cdaae3ef05cffc729a38292ac35596
SmileShmily <zyxjyaya@gmail.com>
2017-06-28 20:42:14 -0500

    css&facebook
    Developed web front end for new container quote feature: gathering
    requirement and
    creating functional specification

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+$webname='JieXu shop';
+$webkey='PHP,shop';
+$weburl='https://github.com/7jx';
+$webcopy='JieXu shop 9-10-2015';
+$webDescription='Mary Xu-jie xu shop';
+$webuserreg='N';
+$webtel='4053349886';
+$webguest='N';
+$userlogin='N';
+$webTitle='Jie Xu Shop';
+$reguser='Y';
+$webdir='/shop/';
+?>
