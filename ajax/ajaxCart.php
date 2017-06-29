e91d419944387ac3fc260f7565aee261e940b98f
SmileShmily <zyxjyaya@gmail.com>
2017-06-28 20:13:19 -0500

    Shopping system
    Designed page layout including input and output implemented using Ajax
    framework

---
+++ ajax/ajaxCart.php
@@ -0,0 +1,40 @@
+<?php
+
+require "../public/init.php";
+
+
+
+if(!ISLOGIN)
+{
+    echo "nologin";
+    exit;
+}
+$cart = new cart();
+
+$id=$_POST["id"];
+$sum=$_POST["sum"];
+
+
+$sql="select * from product where id='$id'";
+$result=$db->select($sql,1);
+
+if(empty($result))
+{
+   echo 0;
+   exit;
+}
+
+if($sum>$result["kucun"])
+    {
+      echo 2;
+      exit;
+    }
+ 
+        
+        
+if($cart->addCart($id,$sum)){
+    echo 1;
+}
+ 
+
+?>
